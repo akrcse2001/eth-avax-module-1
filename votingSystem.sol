3cf3be480513aa836702059ac79a205bfb50574f
akrcse2001 <156458347+akrcse2001@users.noreply.github.com>
2024-01-17 20:14:02 +0530

    Update votingSystem.sol

--- votingSystem.sol
+++ votingSystem.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.0;
 
 contract VotingSystem {
 
-    mapping(address => uint256) public voteMap;
+    mapping(address => uint256) public vote_party;
+    mapping(address => bool ) public  voteMap;
     
     function vote(uint256 voterAge, uint256 partyCode ) public {
         require(voterAge>=18, "age must be greater than 18");
 
-        if( partyCode<1000 && partyCode>9999 )
-            revert("party code should be of 4 digits");
+        if( voteMap[msg.sender] )
+            revert("voter has already voted");
 
-        voteMap[msg.sender] = partyCode;
+        assert(partyCode>=1000 && partyCode<=9999);
+
+        voteMap[msg.sender] = true;
+        vote_party[msg.sender] = partyCode;
     }
 }
